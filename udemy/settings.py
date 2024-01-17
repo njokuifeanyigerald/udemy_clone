cec72d298dac64b42ea4a1c580031fa2620cfd6b
njokuifeanyigerald <brainboyrichmond@gmail.com>
2024-01-17 14:24:20 +0100

    added variables to settings.py then dependencies on pipfile and requirements.txt

--- udemy/settings.py
+++ udemy/settings.py
@@ -10,13 +10,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lxetif-4jrj5$4)8hf2_vfbq2@binh0itj605ivu9v0k5l2w_)'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','udemy-9r2k.onrender.com']
 
 # Application definition
 
